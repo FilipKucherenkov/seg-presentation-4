fb420655e2bc761355f15c878d654db7384f563c
sandi6842@gmail.com <hercogit12>
2020-12-02 17:19:50 +0000

    Push files

---
+++ src/main/java/Main.java
@@ -0,0 +1,17 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import javax.swing.border.*;
+import java.util.Set;
+import java.util.Random;
+import java.util.ArrayList;
+
+
+public class Main
+{
+	public static void main(String[] args) {
+		//initializing the new board
+		Board board = new Board();
+		Activity activity = new Activity();
+	}
+}
